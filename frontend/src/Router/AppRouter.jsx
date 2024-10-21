70b872f263b6e948b66aa44548fa162de36c5ef9
Rakesh Wagh <rakeshw@cybage.com>
2024-10-21 19:50:19 +0530

    Login page added in frontend

--- frontend/src/Router/AppRouter.jsx
+++ frontend/src/Router/AppRouter.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import { BaseLayout } from "../Layout/BaseLayout";
 import { Login } from "../Pages/Login/Login";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route element={<BaseLayout />}>
-        <Route path="/" element={<Login />} />
-      </Route>
+      <Route path="/" element={<Login />} />
     </Routes>
   );
 };
